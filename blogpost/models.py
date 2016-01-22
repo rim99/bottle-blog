c0c429b3db10fcf44e53ce821ae1bd431ba637cc
Zhang, Xin <zhangxin@outlook.com>
2016-01-22 20:22:56 +0800

    add responsive page feature

--- blogpost/models.py
+++ blogpost/models.py
@@ -26,7 +26,7 @@ DATABASE_NAME = 'BlogDatabase'
 HOST = 'localhost'
 USER_NAME = 'rim99'
 PASSWORD = 'passwd'
-DOMAIN_NAME = 'http://localhost:8080'
+DOMAIN_NAME = 'http://127.0.0.1:8080'
 
 class BlogPost(object):
 
@@ -35,8 +35,8 @@ class BlogPost(object):
         self.category = category
         self.content = content
         self.blog_id = blog_id
-        self.post_date = datetime.date.today().strftime("%y-%m-%d")
-        self.url = ('%s/%s' % (DOMAIN_NAME, blog_id))
+        self.post_date = datetime.date.today().strftime("%B %d, %Y")
+        self.url = ('%s/blogpost/%s' % (DOMAIN_NAME, blog_id))
 
     def print(self):
         print('title:%s \ncategory:%s \ndatetime:%s \ncontent:%s \nURL:%s \nblogID:%s' %
@@ -48,7 +48,7 @@ class BlogPost(object):
         cursor = dbconnection.cursor()
         try:
             cursor.execute(
-                "INSERT INTO blogpost (title, catagory, content, blogID, postdate, url) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" %
+                "INSERT INTO blogpost (title, category, content, blogID, postdate, url) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" %
                 (self.title, self.category, self.content, self.blog_id, self.post_date, self.url)
             )
         except:
@@ -71,14 +71,46 @@ class BlogPost(object):
             tmp = cursor.fetchone()
             blog_post = BlogPost(tmp[1], tmp[2], tmp[3], tmp[4])
             blog_post.post_date = tmp[5]
-            dbconnection.commit()
-            cursor.close()
-            dbconnection.close()
+            blog_post.url = tmp[6]
+            # dbconnection.commit()
+            # cursor.close()
+            # dbconnection.close()
             return blog_post
         except:
             print('Error when quering')
             # raise 404
+        finally:
+            dbconnection.commit()
+            cursor.close()
+            dbconnection.close()
 
+    @classmethod
+    def queryByTag(cls, tag):
+        dbconnection = psycopg2.connect("dbname=%s user=%s"
+                                    % (DATABASE_NAME, USER_NAME))
+        cursor = dbconnection.cursor()
+        try:
+            cursor.execute(
+                "SELECT * FROM blogpost WHERE category = '%s';" % tag
+            )
+            blog_list = cursor.fetchall()
+            blogposts = []
+            for tmp in blog_list:
+                blog_post = BlogPost(tmp[1], tmp[2], tmp[3], tmp[4])
+                blog_post.post_date = tmp[5]
+                blog_post.url = tmp[6]
+                blogposts.append(blog_post)
+            # dbconnection.commit()
+            # cursor.close()
+            # dbconnection.close()
+            return blogposts
+        except:
+            print('Error when quering by tag: %s' % tag)
+            # raise 404
+        finally:
+            dbconnection.commit()
+            cursor.close()
+            dbconnection.close()
     @classmethod
     def delete(cls, blog_id):
         dbconnection = psycopg2.connect("dbname=%s user=%s"
@@ -90,17 +122,50 @@ class BlogPost(object):
             )
         except:
             print('Error when deleting')
-        dbconnection.commit()
-        cursor.close()
-        dbconnection.close()
+        finally:
+            dbconnection.commit()
+            cursor.close()
+            dbconnection.close()
         return
 
-print('1')
-m = BlogPost('new post', 'Python', 'This is just a test post!', 'TEST')
-m.save()
-print('2')
-a = BlogPost.query('TEST')
-a.print()
-print('3')
-#BlogPost.delete('TEST')
-c = BlogPost.query('TEST')
+    @classmethod
+    def getAll(cls):
+        dbconnection = psycopg2.connect("dbname=%s user=%s"
+                                    % (DATABASE_NAME, USER_NAME))
+        cursor = dbconnection.cursor()
+        try:
+            cursor.execute(
+                "SELECT * FROM blogpost;"
+            )
+            blog_list = cursor.fetchall()
+            blogposts = []
+            for tmp in blog_list:
+                blog_post = BlogPost(tmp[1], tmp[2], tmp[3], tmp[4])
+                blog_post.post_date = tmp[5]
+                blog_post.url = tmp[6]
+                blogposts.append(blog_post)
+            # dbconnection.commit()
+            # cursor.close()
+            # dbconnection.close()
+            return blogposts
+        except:
+            print('Error when getting all')
+        finally:
+            dbconnection.commit()
+            cursor.close()
+            dbconnection.close()
+
+# 没必要存储URL
+# print('save & fectch all\n')
+# m = BlogPost('第七篇测试', 'Python', '这是一片简单的测试文章!\n由于bottle.py的简单，可以任意的使用。但是对工程目录进行良好的规划，可以为后续的维护带来便利。由于之前对Django比较熟悉，参考Django工程目录设计了 bottle.py应用的目录结构：', 'TEST3')
+# m.save()
+# s = BlogPost.getAll()
+# print(s,'\nlen : ',len(s))
+# print('querying...\n')
+# a = BlogPost.query('TEST')
+# a.print()
+# print('avoid delete...')
+# for i in range(11):
+# BlogPost.delete('插入排序与希尔排序')
+# c = BlogPost.query('ss')
+# print(c.url)
