cc9047ca40da9718b5ca292643c94df8df6696cc
rim99 <zhangxin086@gmail.com>
2016-11-18 23:50:10 +0800

    Enhance database querying efficiency

--- blogpost.py
+++ blogpost.py
@@ -32,22 +32,23 @@ class BlogPost(object):
         return BlogPost(result[1], result[2], result[3], result[4], result[5], result[6])
 
     @classmethod
-    def get_sql_cmd(cls, total, key_word='', attachment=None, page_num=1):
+    def get_sql_cmd(cls, total=0, key_word='', attachment=None, page_num=1):
         '''Str, Str or Blogpost -> Str
         Return SQL command according to the key_word.
         '''
         if key_word == "query_by_id":
             return "SELECT * FROM blogpost WHERE blogID = '{0}';".format(attachment)
         elif key_word == "query_by_tag":
-            return "SELECT * FROM blogpost WHERE category = '{0}' AND ID > {1} AND ID <= {2} \
-            ORDER BY postdate DESC;".format(
+            return "(SELECT * FROM blogpost WHERE category = '{0}' ORDER BY postdate DESC LIMIT {1}) EXCEPT \
+             (SELECT * FROM blogpost WHERE category = '{0}' ORDER BY postdate DESC LIMIT {2});".format(
                 attachment,
-                max(0, total - page_num * POSTS_COUNT_PER_PAGE),
-                total - (page_num - 1) * POSTS_COUNT_PER_PAGE)
+                page_num * POSTS_COUNT_PER_PAGE,
+                min(total, (page_num - 1) * POSTS_COUNT_PER_PAGE))
         elif key_word == "get_all":
-            return "SELECT * FROM blogpost WHERE ID > {0} AND ID <= {1} ORDER BY postdate DESC;".format(
-                max(0, total - page_num * POSTS_COUNT_PER_PAGE),
-                total - (page_num - 1) * POSTS_COUNT_PER_PAGE)
+            return "(SELECT * FROM blogpost ORDER BY postdate DESC LIMIT {0})\
+             EXCEPT (SELECT * FROM blogpost ORDER BY postdate DESC LIMIT {1});".format(
+                page_num * POSTS_COUNT_PER_PAGE,
+                min(total, (page_num - 1) * POSTS_COUNT_PER_PAGE))
         elif key_word == "delete_by_id":
             return "DELETE FROM blogpost WHERE blogID = '{}';".format(attachment)
         elif key_word == "save" and isinstance(attachment, BlogPost):
