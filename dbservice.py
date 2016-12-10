ca3978b4bb061dd1c8451a950f5f9e239f775b49
rim99 <zhangxin086@gmail.com>
2016-12-10 20:03:41 +0800

    Rewrite ASYNC database query service

--- dbservice.py
+++ dbservice.py
@@ -3,7 +3,10 @@
 
 __author__ = 'Rim99'
 
-import psycopg2, concurrent.futures, threading
+import psycopg2
+from asyncio import sleep, get_event_loop
+from select import select
+from psycopg2.pool import SimpleConnectionPool
 
 DATABASE_NAME = 'BlogDatabase'
 HOST = 'localhost'
@@ -23,26 +26,64 @@ class Task(object):
         self.send_conn = send_conn
         self.result_type = result_type
 
+class AsyncConnectionPool(SimpleConnectionPool):
+    '''Rewrite a async blocking version
+        The object contains the following params:
+            1. self._pool  # contains available conn
+            2. self._used  # conatins the using conn
+            3. self._rused  # id(conn) -> key map
+            4. self._keys
+    '''
+    async def getconn(self, key=None):
+        """Get a free connection and assign it to 'key' if not None."""
+        if self.closed:
+            raise Exception("Connection pool is closed")
+        if key is None:
+            key = self._getkey()
+        if key in self._used:
+            return self._used[key]
+        while True:
+            if self._pool:
+                # add the new conn into the Dict: _used;
+                # new conn is popped from the available list:_pool
+                self._used[key] = conn = self._pool.pop()
+                self._rused[id(conn)] = key
+                return conn
+            await sleep(0.1) # LOW EFFICIENCY
+
+async def ready(conn):
+    while True:
+        state = conn.poll()
+        if state == psycopg2.extensions.POLL_OK:
+            break
+        elif state == psycopg2.extensions.POLL_WRITE:
+            select([], [conn.fileno()], [])
+        elif state == psycopg2.extensions.POLL_READ:
+            select([conn.fileno()], [], [])
+        else:
+            raise psycopg2.OperationalError("poll() returned %s" % state)
+
+async def process_task(pool, task):
+    conn = await pool.getconn()
+    await ready(conn)
+    acurs = conn.cursor()
+    jobs_dict = {'obj': acurs.fetchone,
+                 'list': acurs.fetchall}
+    acurs.execute(task.sql_cmd)
+    await ready(acurs.connection)
+    result = jobs_dict[task.result_type]()
+    task.send_conn.send(result)
+    pool.putconn(conn)
 
 def db_query_service(task_queue, thread_num):
-    dbconn = psycopg2.connect(database=DATABASE_NAME, user=USER_NAME)
-    lock = threading.Lock()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=thread_num):
-        cursor = dbconn.cursor()
-        jobs_dict = {'obj': cursor.fetchone,
-                     'list': cursor.fetchall}
+    pool = AsyncConnectionPool(minconn=1, maxconn=4, database=DATABASE_NAME, user=USER_NAME, async=True)
+    loop = get_event_loop()
+    while True:
         try:
-            while True:
-                lock.acquire()
-                task = task_queue.get()
-                lock.release()
-                cursor.execute(task.sql_cmd)
-                result = jobs_dict[task.result_type]()
-                task.send_conn.send(result)
+            task = task_queue.get()
+            loop.run_until_complete(process_task(pool, task))
         except Exception as msg:
             print(msg)
-            cursor.close()
-    dbconn.close()
 
 def db_update(sql_cmd):
     dbconn = psycopg2.connect(database=DATABASE_NAME, user=USER_NAME)
